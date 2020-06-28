7d1b832c1f1f81b6e82709862d38a1dd4d6f58bd
John Tayse <johntayse@Johns-MacBook-Pro.local>
2020-06-28 19:15:25 -0400

    removing spotify link

--- player/lib/src/constants.dart
+++ player/lib/src/constants.dart
@@ -1,2 +1,2 @@
 const kPodcastFeed = 'https://johntayse.github.io/JustTryDIYPodcast/feed.xml';
-const kSpotifyLink = 'https://open.spotify.com/show/3UTiK34aDOOSHFpGQ0RglN';
+const kSpotifyLink = 'https://open.spotify.com/show/';

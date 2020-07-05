a9bbd19d7dbb5187bf4cde108d4538609cc04016
JohnTayse <JohnTayse@users.noreply.github.com>
2020-07-05 15:03:59 -0400

    Update constants.dart
    Changed Google feed link

--- player/lib/src/constants.dart
+++ player/lib/src/constants.dart
@@ -1,4 +1,4 @@
 const kPodcastFeed = 'https://johntayse.github.io/JustTryDIYPodcast/feed.xml';
 const kiTunesLink = 'https://podcasts.apple.com/us/podcast/feed/id1521176717';
 const kSpotifyLink = 'https://open.spotify.com/show/36F8tB4oLo8fSj68G97YEh';
-const kGoogleLink = 'https://podcasts.google.com/feed/3AWEgzMAYrC1nYVm9Z4v6A7g';
+const kGoogleLink = 'https://podcasts.google.com/feed/aHR0cHM6Ly9qb2hudGF5c2UuZ2l0aHViLmlvL0p1c3RUcnlESVlQb2RjYXN0L2ZlZWQueG1s';
